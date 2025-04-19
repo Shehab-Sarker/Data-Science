8c3c286b172ad0bb74d49abdb42bbf3059738247
Shehab218 <sarkershehab123@gmail.com>
2025-04-20 00:28:19 +0600

    assignment

---
+++ Assignment/assin_3/prob.py
@@ -0,0 +1,20 @@
+# Advanced-Data Structures and Comparison
+# Write a Python program that takes two sets from the user and computes their union, intersection, and difference.
+
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+print("umion :",A|B)
+print("intersection :",A&B)
+print("difference(A-B) :",A-B)
+print("difference(B-A) :",B-A)
+
+print()
+
+print("umion :",A.union(B))
+print("intersection :",A.intersection(B))
+print("difference(A-B) :",A.difference(B))
+print("difference(B-A) :",B.difference(A))
+
+
+
